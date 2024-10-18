7cfbf30bf11b3086631a012f914b5bd9023184a6
igor y <yakobchuk.igor@yahoo.com>
2024-10-18 21:25:29 +0300

    +1 led

--- src/svetodiod.cpp
+++ src/svetodiod.cpp
@@ -4,12 +4,16 @@ const int led1 = 2;
 const int led2 = 4;  
 const int led3 = 18; 
 const int led4 = 19;
+const int led5 = 22;
+
+const int delayTime = 300;
 
 void setup() {
     pinMode(led1, OUTPUT); 
     pinMode(led2, OUTPUT);  
     pinMode(led3, OUTPUT); 
     pinMode(led4, OUTPUT);
+    pinMode(led5, OUTPUT);
 }
 
 void loop() {
@@ -18,24 +22,36 @@ void loop() {
     digitalWrite(led2, LOW);
     digitalWrite(led3, LOW);
     digitalWrite(led4, LOW);
-    delay(500);   
+    digitalWrite(led5, LOW);
+    delay(delayTime);
+      
 
     digitalWrite(led1, LOW);
     digitalWrite(led2, HIGH);
     digitalWrite(led3, LOW);
     digitalWrite(led4, LOW);
-    delay(500);  
+    digitalWrite(led5, LOW);
+    delay(delayTime);
 
     digitalWrite(led1, LOW);
     digitalWrite(led2, LOW);
     digitalWrite(led3, HIGH);
     digitalWrite(led4, LOW);
-    delay(500);  
+    digitalWrite(led5, LOW);
+    delay(delayTime);
 
     digitalWrite(led1, LOW);
     digitalWrite(led2, LOW);
     digitalWrite(led3, LOW);
     digitalWrite(led4, HIGH);
-    delay(500);  
+    digitalWrite(led5, LOW);
+    delay(delayTime);  
     
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, HIGH);
+    delay(delayTime); 
+
 } 
