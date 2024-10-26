8ab5f1dec30fe41e86c104d617e00b8ab3fadba2
igor y <yakobchuk.igor@yahoo.com>
2024-10-27 02:00:21 +0300

    +3LEDs

---
+++ .pio/build/esp32doit-devkit-v1/FrameworkArduino/USBCDC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32doit-devkit-v1/FrameworkArduino/USBCDC.cpp.o: \
+ /Users/igoryakobchuk/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBCDC.cpp \
+ /Users/igoryakobchuk/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
+ /Users/igoryakobchuk/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
--- src/svetodiod.cpp
+++ src/svetodiod.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-const int leds_array[] = {2, 4, 18, 19, 22};
+const int leds_array[] = {2, 4, 18, 19, 22, 23, 26, 27};
 const int num_leds = sizeof(leds_array) / sizeof(leds_array[0]); 
 const int delayTime = 1000;
 
